04f70ea8326aa12a423653f9e252592abd9bb973
Trystan Partridge <partridget9@gmail.com>
2018-07-17 08:18:04 -0400

    Revision Update

--- src/clientversion.h
+++ src/clientversion.h
@@ -40,8 +40,8 @@
 // Modify version number here!!!
 #define CLIENT_VERSION_MAJOR 1
 #define CLIENT_VERSION_MINOR 3
-#define CLIENT_VERSION_REVISION 3
-#define CLIENT_VERSION_BUILD 1
+#define CLIENT_VERSION_REVISION 4
+#define CLIENT_VERSION_BUILD 0
 
 
 /**
