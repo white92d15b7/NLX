d05677bdd989f5e3b40bcdad9e0657ddb12a04db
Trystan Partridge <partridget9@gmail.com>
2019-05-12 22:40:24 -0400

    condensing file

--- share/genbuild.sh
+++ share/genbuild.sh
@@ -17,15 +17,12 @@ DESC=""
 SUFFIX=""
 LAST_COMMIT_DATE=""
 if [ -e "$(which git 2>/dev/null)" -a "$(git rev-parse --is-inside-work-tree 2>/dev/null)" = "true" ]; then
-    # clean 'dirty' status of touched files that haven't been modified
     git diff >/dev/null 2>/dev/null 
 
-    # if latest commit is tagged and not dirty, then override using the tag name
     DESC=$(git describe 2>/dev/null)
     RAWDESC=$(git describe --abbrev=0 2>/dev/null)
     git diff-index --quiet HEAD -- || DESC="v1.1"
 
-    # get a string like "2012-04-10 16:27:19 +0200"
     LAST_COMMIT_DATE="$(git log -n 1 --format="%ci")"
 fi
 
@@ -35,7 +32,6 @@ else
     NEWINFO="// No build information available"
 fi
 
-# only update build.h if necessary
 if [ "$INFO" != "$NEWINFO" ]; then
     echo "$NEWINFO" >"$FILE"
     if [ -n "$LAST_COMMIT_DATE" ]; then
